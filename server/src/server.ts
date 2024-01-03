3228a7797668f3a850faf3343d44e120fc33c0f5
tiagoaraujo00 <tiago81451@gmail.com>
2024-01-03 10:17:10 -0300

    feat: implement GET route to list all registered habits

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import Fastify from "fastify";
+import { PrismaClient } from '@prisma/client'
 
 const app = Fastify();
+const prisma = new PrismaClient()
 
 app.get("/", () => {
-  return 'Hello'
+  const habits = prisma.habit.findMany()
+  return habits;
 });
 
 app.listen({
