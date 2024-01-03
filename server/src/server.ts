df52d02c1189e01eb9f80f86081b62ce35f3421c
tiagoaraujo00 <tiago81451@gmail.com>
2024-01-03 09:52:38 -0300

    chore: install Prisma dependencies

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,14 @@
-console.log('oie');
+import Fastify from "fastify";
+
+const app = Fastify();
+
+app.get("/", () => {
+  return 'Hello'
+});
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('Server running!');
+  
+})
