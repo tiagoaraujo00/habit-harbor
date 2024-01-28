5d0e92ca32c847218eed03f172218d5c5d44209d
tiagoaraujo00 <tiago81451@gmail.com>
2024-01-27 22:11:03 -0300

    feat: post habit route done

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,16 @@
 import Fastify from "fastify";
-import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client'
+import cors from "@fastify/cors";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
+app.register(appRoutes)
 
-app.get("/", () => {
-  const habits = prisma.habit.findMany();
-  return habits;
-});
-
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('Server running!');
-  
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("Server running!");
+  });
