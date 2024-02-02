38d298e967d014be88908905c80b6395890c1404
tiagoaraujo00 <tiago81451@gmail.com>
2024-02-02 19:35:58 -0300

    feat: get day route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -30,4 +30,47 @@ export async function appRoutes(app: FastifyInstance) {
       }
     })
   });
+
+  app.get('/day', async(request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date()
+    })
+
+    const { date } = getDayParams.parse(request.query)
+
+    const weekDay = dayjs(date).get('day')
+    const parsedDate = dayjs(date).startOf('day')
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay
+          }
+        }
+      }
+    })
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      }
+    })
+
+    const completedHabits = day?.dayHabits.map(dayHabit => {
+      return dayHabit.habit_id
+    })
+
+    return {
+      possibleHabits,
+      completedHabits
+    }
+
+  })
 }
