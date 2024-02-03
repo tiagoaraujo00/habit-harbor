fbe4cd940b6dd2306729529c0713162e2d4adef4
tiagoaraujo00 <tiago81451@gmail.com>
2024-02-03 19:54:24 -0300

    feat: generateDatesFromYearBeginning function implemented

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,6 +1,11 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 import HabitDay from "./HabitDay";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
+const summaryDates = generateDatesFromYearBeginning();
+console.log(summaryDates);
+
 
 const SummaryTable = () => {
   return (
@@ -8,7 +13,10 @@ const SummaryTable = () => {
       <div className="grid grid-flow-row gap-3 grid-rows-7">
         {weekDays.map((day, index) => {
           return (
-            <div key={index} className="flex items-center justify-center w-10 h-10 text-xl font-bold text-zinc-400">
+            <div
+              key={index}
+              className="flex items-center justify-center w-10 h-10 text-xl font-bold text-zinc-400"
+            >
               {day}
             </div>
           );
